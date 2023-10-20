302bbf1701463d0e2ad6f15556a23905881ba296
siyamar <ab.rahmansiyam@gmail.com>
2023-10-20 08:47:17 +0600

    added port and index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Brand Shop Server is Running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Brand Shop is Running on port: ${port}`)
+})
